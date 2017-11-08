10e725e2d973074fb8e9f4bcf99ed0fbc76848d8
wsx90315 <wsx90315@gmail.com>
2017-11-08 07:19:43 -0600

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,7 +1,7 @@
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("databaseuser");
+$dbuser = getenv("databaseusr");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
 
